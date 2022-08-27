94f161960179f048dfa37fdb3e3cffb9652adbb1
mohehsan <muhammadihsan3011@gmail.com>
2022-08-27 12:41:41 +0700

    update

--- pages/views/enterprise-social-media-advertising-services.tsx
+++ pages/views/enterprise-social-media-advertising-services.tsx
@@ -161,16 +161,16 @@ const Page: NextPage = () => {
 							<CardContent className="space-y-9">
 								{
 									pricing.content.map((content) => (
-										<IconItem icon={content.icon} title={content.title} className="mt-4"/>
+										<IconItem icon={content.icon} title={content.title} className="mt-4" />
 									))
 								}
 							</CardContent>
 							<div className="px-4 py-10 mt-4 flex items-center justify-center flex-col">
-								<p className="border-t border-[#000] w-full mt-1 mb-10 text-center"/>
+								<p className="border-t border-[#000] w-full mt-1 mb-10 text-center" />
 								<p className="mb-10">$3,125 INITIAL INVESTMENT</p>
 								<Button
-									bgColor="bg-[#000]" 
-									textColor="text-white" 
+									bgColor="bg-[#000]"
+									textColor="text-white"
 									className="py-4 px-10 text-[-0.8em]">
 									Send Proposal
 								</Button>
@@ -179,10 +179,36 @@ const Page: NextPage = () => {
 					))}
 				</div>
 			</Section>
-			
-			<Section className="text-center pt-16">
+
+			<Section className="pt-16">
 				<div className="flex py-2 space-y-9 justify-center">
-					<p className="flex text-4xl font-medium text-sky-500">See Full Deliverables<BsChevronRight className="ml-10"/></p>
+					<p className="flex text-4xl font-medium text-sky-500">See Full Deliverables<BsChevronRight className="ml-10" /></p>
+				</div>
+			</Section>
+
+			<Section className="pt-16">
+				<div className="flex py-2 space-y-16 justify-center flex-col">
+					<h1 className="flex text-4xl text-justify">Turn followers into customers with Muscled’s social media advertising</h1>
+					<div className="text-justify text-3xl">
+						<p className="mb-20">
+							Every digital marketing strategy has to have social media, that means yours too.
+						</p>
+						<p className="mb-10">
+							With platforms like Facebook, LinkedIn, Twitter, Instagram, and even TikTok, your business can go to wherever it needs to, whether to boost your recruitment efforts, sales ventures, or customer loyalty incentives.
+						</p>
+						<p className="mb-10">
+							While <a href="" className="text-sky-400">social media marketing</a> does help reach these goals, advertising kickstarts your efforts by adding more OOMPH to your company’s presence in social media platforms. With social media ads that hit the spot in targeting, messaging, and bidding, your enterprise business can get phenomenal results from social media.
+						</p>
+						<p className="mb-10">
+							At Muscled, our social media management team specializes in corporate-level social media advertising.
+						</p>
+						<p className="mb-20">
+							When you partner with our own social media experts, you get a complete social media advertising package. From curating your strategy to creating your ads to managing your campaigns, we cover everything in this aspect. Plus, we provide you with reports that are convenient and accurate.
+						</p>
+						<p>
+							If you want to learn how our Meta Business Partner agency can help bring more power to your brand — and why 91% of brands make us their long-term partner — <a href="" className="text-sky-400">contact us online</a> or call us at ######## to speak with an experienced strategist!
+						</p>
+					</div>
 				</div>
 			</Section>
 		</div>
